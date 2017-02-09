f70eb40a15924e0c4221ec18fba69ca3aa9d7c84
Gena Serey <genaserey@gmail.com>
2017-02-08 19:07:21 -0600

    RPS game add

--- media/greeting.js
+++ media/greeting.js
@@ -2,3 +2,18 @@ var name = prompt('Hiya! What is your name?');
 var output = document.querySelector('#greeting');
 
 output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
+
+var btn = document.querySelector('#userButton');
+
+var richardFood = "sushi";
+var simoneFood = "Pho";
+var genaFood = "pasta";
+
+var myFoodArray = ["Tacos", "Burriotos", richardFood, "curry", simoneFood, genaFood];
+
+var classCity =
+{
+  "gena": "Ashland", "rebecca": "Memphis", "wayne": "Manchester", "richard": "Galveston", "simone": "Louisville"
+}
+
+//Access your city and display it in <p></p> inside a div with the id “myCity” on your media page.
